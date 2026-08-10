95f5f0eb1140a84272170f044c2c39a1aad1795b
Belceb45 <rubiodiego001@gmail.com>
2026-08-09 23:13:14 -0600

    CORSCONFIG2

--- src/main/java/org/hospital/backendpacientes/CorsConfig.java
+++ src/main/java/org/hospital/backendpacientes/CorsConfig.java
@@ -1,25 +1,32 @@
 package org.hospital.backendpacientes;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        // IMPORTANTE: Cambia esta URL por tu link real de Vercel
-                        .allowedOrigins("https://sistema-hospital-frontend.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public FilterRegistrationBean<CorsFilter> customCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        // Tu URL de Vercel (sin diagonal al final)
+        config.addAllowedOrigin("https://sistema-hospital-frontend.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+
+        // ESTA ES LA MAGIA: Le da a este filtro la máxima prioridad
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
