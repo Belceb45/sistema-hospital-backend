66af371f4f1ed6459cf377b6bb769d9e3c110fbc
Belceb45 <rubiodiego001@gmail.com>
2026-08-09 22:58:28 -0600

    CORSCONFIG

---
+++ src/main/java/org/hospital/backendpacientes/CorsConfig.java
@@ -0,0 +1,25 @@
+package org.hospital.backendpacientes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // IMPORTANTE: Cambia esta URL por tu link real de Vercel
+                        .allowedOrigins("https://sistema-hospital-frontend.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
